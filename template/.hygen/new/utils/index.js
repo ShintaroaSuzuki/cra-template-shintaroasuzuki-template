bd6e18e0ea4f8629a3a317ded1a68a6ec6292112
shintaroasuzuki <shinbo.dorapion19971221@gmail.com>
2023-03-30 11:50:05 +0900

    fix: :lady_beetle: hygen の設定ファイルを修正

--- template/.hygen/new/utils/index.js
+++ template/.hygen/new/utils/index.js
@@ -1,26 +1,26 @@
-const fs = require("fs");
+const fs = require('fs');
 
 const features = fs
-    .readdirSync("./src/features")
-    .map((dir_name) => `src/features/${dir_name}/components`);
+    .readdirSync('./src/features')
+    .map((dir_name) => `src/features/${dir_name}/utils`);
 
 module.exports = {
     prompt: ({ inquirer }) => {
         const questions = [
             {
-                type: "select",
-                name: "dir_name",
-                message: "どこに作成しますか？",
-                choices: ["src/components", ...features],
+                type: 'select',
+                name: 'dir_name',
+                message: 'どこに作成しますか？',
+                choices: ['src/utils', ...features]
             },
             {
-                type: "input",
-                name: "function_name",
-                message: "関数名を入力してください",
-            },
+                type: 'input',
+                name: 'function_name',
+                message: '関数名を入力してください'
+            }
         ];
         return inquirer.prompt(questions).then((answers) => {
             return answers;
         });
-    },
+    }
 };
