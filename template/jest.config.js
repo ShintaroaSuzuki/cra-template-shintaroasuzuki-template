35303bd9ead60e63b0adc759992170926e7044f7
shintaroasuzuki <shinbo.dorapion19971221@gmail.com>
2023-04-05 10:39:02 +0900

    test: :test_tube: jest の設定を追加

--- template/jest.config.js
+++ template/jest.config.js
@@ -1,34 +1,35 @@
 module.exports = {
     transform: {
-        '^.+\\.ts[x]?$': 'ts-jest'
+        "^.+\\.ts[x]?$": "ts-jest",
     },
     moduleNameMapper: {
-        '#(.*)': '<rootDir>/node_modules/$1',
-        '@/(.*)': '<rootDir>/src/$1'
+        "#(.*)": "<rootDir>/node_modules/$1",
+        "@/(.*)": "<rootDir>/src/$1",
     },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    preset: 'ts-jest/presets/default-esm',
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+    preset: "ts-jest/presets/default-esm",
     collectCoverageFrom: [
-        'src/utils/**/*.{js,jsx,ts,tsx}',
-        '!src/utils/index.ts',
-        '!src/utils/**/index.ts',
-        'src/hooks/**/*.{js,jsx,ts,tsx}',
-        '!src/hooks/index.ts',
-        '!src/hooks/**/index.ts',
-        'src/features/**/hooks/**/*.{js,jsx,ts,tsx}',
-        '!src/features/**/hooks/index.ts',
-        '!src/features/**/hooks/**/index.ts'
+        "src/utils/**/*.{js,jsx,ts,tsx}",
+        "!src/utils/index.ts",
+        "!src/utils/**/index.ts",
+        "src/hooks/**/*.{js,jsx,ts,tsx}",
+        "!src/hooks/index.ts",
+        "!src/hooks/**/index.ts",
+        "src/features/**/hooks/**/*.{js,jsx,ts,tsx}",
+        "!src/features/**/hooks/index.ts",
+        "!src/features/**/hooks/**/index.ts",
     ],
     coverageReporters: [
-        ['text', { file: 'coverage.txt' }],
-        ['json-summary', { file: 'coverage-summary.json' }]
+        ["text", { file: "coverage.txt" }],
+        ["json-summary", { file: "coverage-summary.json" }],
     ],
     coverageThreshold: {
         global: {
             branches: 100,
             functions: 100,
             lines: 100,
-            statements: 100
-        }
-    }
+            statements: 100,
+        },
+    },
+    testEnvironment: "jest-environment-jsdom",
 };
